16eb84fc80a8c9e6727c2d116ebab8f00d2b3bf7
magujun <guimaraesjunior.marcelo@gmail.com>
2021-03-22 12:28:25 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,4 @@ import { app } from '.';
 const PORT = 3333;
 
 app.get('/', (request, response) => response.json({ message: '⚡ Node server is running on port: ' + PORT + ' ⚡' }));
-
 app.listen(PORT, () => console.log('⚡Node server is running on port: ' + PORT + ' ⚡'));
